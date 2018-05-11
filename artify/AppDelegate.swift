2a8c02207d2bbd2005d4ffd5a5ca60cb03ee4de2
Nghia Tran <vinhnghiatran@gmail.com>
2018-05-11 15:17:05 +0700

    Initial Commit

---
+++ artify/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  artify
+//
+//  Created by Nghia Tran on 5/11/18.
+//  Copyright © 2018 com.art. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ artify/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  artify
+//
+//  Created by Nghia Tran on 5/11/18.
+//  Copyright © 2018 com.art. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
